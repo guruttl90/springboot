701b2c216eea8e8e52076dcdb1d03e3273ce87f7
guru <guru@host.docker.internal>
2020-06-25 11:17:15 +0530

    demo exception handling module added

---
+++ demo-exception-handling/src/main/java/com/spring/demoexceptionhandling/DemoExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.demoexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExceptionHandlingApplication.class, args);
+	}
+
+}
