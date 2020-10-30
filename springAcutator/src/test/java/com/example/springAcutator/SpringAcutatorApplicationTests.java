43848e90189ab592e9f1f9ce47388ee67b7a59ec
GURU <GURU@DESKTOP-C8MNP18>
2020-10-30 11:05:09 +0530

    spring acutator project

---
+++ springAcutator/src/test/java/com/example/springAcutator/SpringAcutatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springAcutator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcutatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
